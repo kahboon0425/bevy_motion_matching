c4c3e80a6ae6f7bae4aad26f692e0ace2b4f7b21
ConquerorFY <ryanrlfy33@gmail.com>
2023-12-05 23:25:42 +0800

    Load BVH files into Bevy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
-use bevy::prelude::*;
+use bvh_anim;
+use std::error::Error;
+use std::fs;
+use std::io;
 
-fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+fn main() -> Result<(), Box<dyn Error>> {
+    let animation_file_path = "./assets/walking-animation-dataset/";
+
+    if let Ok(entries) = fs::read_dir(animation_file_path) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                if let Some(file_name) = entry.file_name().to_str() {
+                    println!("================================== Animation File: {} ==================================", file_name);
+                    let animation_file_name = animation_file_path.to_owned() + &file_name;
+
+                    let bvh_file = fs::File::open(animation_file_name)?;
+                    let bvh = bvh_anim::from_reader(io::BufReader::new(bvh_file))?;
+
+                    for joint in bvh.joints() {
+                        println!("{:#?}", joint);
+                    }
+
+                    println!("Frame time: {:?}", bvh.frame_time());
+
+                    // for frame in bvh.frames() {
+                    //     println!("{:?}", frame);
+                    // }
+
+                    // let mut out_file = File::create("./out.bvh");
+                    // bvh.write_to(&mut out_file)?;
+                }
+            }
+        }
+        Ok(())
+    } else {
+        Err("Failed to read directory".into())
+    }
 }
