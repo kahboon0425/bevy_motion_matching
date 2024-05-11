986b241f858ddaec62c5e3378e4b74bcece0c21b
Nixon <voxell.technologies@gmail.com>
2024-05-11 11:45:53 +0800

    feat: remove BvhAsset as a RenderAsset

--- src/animation_loader.rs
+++ src/animation_loader.rs
@@ -1,9 +1,4 @@
-use bevy::{
-    asset::{Asset, Handle},
-    ecs::system::SystemParamItem,
-    prelude::*,
-    render::render_asset::{self, RenderAsset, RenderAssetPlugin, RenderAssetUsages},
-};
+use bevy::{asset::Handle, prelude::*};
 use bvh_anim::{errors::LoadError, Bvh};
 use std::{fs, io::BufReader};
 
@@ -11,15 +6,15 @@ pub struct AnimationLoaderPlugin;
 
 impl Plugin for AnimationLoaderPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(RenderAssetPlugin::<BvhAsset>::default())
-            .init_asset::<BvhAsset>()
+        app.init_asset::<BvhAsset>()
+            .insert_resource(BvhHandles::default())
             .add_systems(Startup, store_bvh);
     }
 }
 
-#[derive(Component, Clone, Default)]
-pub struct BvhHandle {
-    pub bvh_assets: Handle<BvhAsset>,
+#[derive(Resource, Clone, Default)]
+pub struct BvhHandles {
+    pub handles: Vec<Handle<BvhAsset>>,
 }
 
 #[derive(Asset, TypePath, Clone, Default)]
@@ -27,48 +22,31 @@ pub struct BvhAsset {
     pub asset: Bvh,
 }
 
-impl RenderAsset for BvhAsset {
-    type PreparedAsset = Self;
-
-    type Param = ();
-
-    fn asset_usage(&self) -> RenderAssetUsages {
-        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD
-    }
-
-    fn prepare_asset(
-        self,
-        _param: &mut SystemParamItem<Self::Param>,
-    ) -> Result<Self::PreparedAsset, render_asset::PrepareAssetError<Self>> {
-        Ok(self)
-    }
-}
-
 pub fn load_bvh() -> Result<Vec<Bvh>, LoadError> {
     let animation_file_path: &str = "./assets/walking-animation-dataset/";
 
     let mut loaded_bvhs: Vec<Bvh> = Vec::new();
 
-    let mut count: usize = 0;
     if let Ok(entries) = fs::read_dir(animation_file_path) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                if let Some(filename) = entry.file_name().to_str() {
-                    let filename: String = animation_file_path.to_owned() + filename;
+        // We only load to entires for now
+        for entry in entries.take(2) {
+            let Ok(entry) = entry else {
+                continue;
+            };
 
-                    let bvh_file: fs::File = fs::File::open(&filename).unwrap();
-                    let bvh_reader: BufReader<fs::File> = BufReader::new(bvh_file);
+            let filename = entry.file_name();
+            let Some(filename) = filename.to_str() else {
+                continue;
+            };
 
-                    let bvh: Bvh = bvh_anim::from_reader(bvh_reader)?;
+            let filename: String = animation_file_path.to_owned() + filename;
 
-                    loaded_bvhs.push(bvh);
+            let bvh_file = fs::File::open(&filename).unwrap();
+            let bvh_reader = BufReader::new(bvh_file);
 
-                    if count >= 2 {
-                        break;
-                    }
-                    count += 1;
-                }
-            }
+            let bvh: Bvh = bvh_anim::from_reader(bvh_reader)?;
+
+            loaded_bvhs.push(bvh);
         }
 
         if loaded_bvhs.is_empty() {
@@ -81,15 +59,14 @@ pub fn load_bvh() -> Result<Vec<Bvh>, LoadError> {
     Ok(loaded_bvhs)
 }
 
-pub fn store_bvh(mut commands: Commands, mut assets: ResMut<Assets<BvhAsset>>) {
+pub fn store_bvh(mut assets: ResMut<Assets<BvhAsset>>, mut bvh_handles: ResMut<BvhHandles>) {
     match load_bvh() {
         Ok(bvhs) => {
             for bvh in bvhs {
                 // Create a new BvhAsset and insert it into the asset server
-                commands.spawn(BvhHandle {
-                    bvh_assets: assets.add(BvhAsset { asset: bvh.into() }),
-                    ..default()
-                });
+                bvh_handles
+                    .handles
+                    .push(assets.add(BvhAsset { asset: bvh.into() }));
             }
         }
         Err(err) => {
