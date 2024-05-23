e95c94b87e6ba5c92892d59b53dca7d1bc4217e1
Nixon <voxell.technologies@gmail.com>
2024-05-23 21:50:40 +0800

    feat: perform exclusive sum scan operation on offsets

--- src/motion_database.rs
+++ src/motion_database.rs
@@ -27,7 +27,7 @@ pub struct MotionData {
     pub trajectories: Vec<TrajectoryPosition>,
     pub trajectory_offsets: Vec<usize>,
     pub poses: Vec<Pose>,
-    pub pose_offset: Vec<usize>,
+    pub pose_offsets: Vec<usize>,
 }
 
 // trajectories: [ bvh0:traj0, bvh0:traj1, bvh0:traj2, bvh1:traj0, bvh1:traj1, bvh2:traj0 ]
@@ -35,8 +35,11 @@ pub struct MotionData {
 
 pub fn extract_motion_data(bvh_asset: &Assets<BvhAsset>, build_config: &mut BuildConfig) {
     let mut motion_data = MotionData::default();
+
+    let mut trajectory_data_len = 0;
+    let mut motion_data_len = 0;
+
     for id in build_config.bvh_assets.iter() {
-        println!("id: {}", id);
         let Some(BvhAsset(bvh)) = bvh_asset.get(*id) else {
             return;
         };
@@ -44,18 +47,15 @@ pub fn extract_motion_data(bvh_asset: &Assets<BvhAsset>, build_config: &mut Buil
         let interval = 0.3333;
         let frame_count = bvh.frames().len();
         let total_duration = bvh.frame_time().as_secs_f32() * frame_count as f32;
-        println!("Frame Length {}", frame_count);
 
-        let mut trajectory_data_len: usize = 0;
+        motion_data.trajectory_offsets.push(trajectory_data_len);
         let mut trajectory_index = 0;
-
         loop {
             let time = interval * trajectory_index as f32;
             if time > total_duration {
                 break;
             }
             let (frame_index, _interp_factor) = get_pose(time, bvh);
-            // let future_frame_index = start_frame + (time / duration_per_frame).round() as usize;
 
             if let Some(future_frame) = bvh.frames().nth(frame_index) {
                 if let Some(hip_joint) = bvh.joints().find(|j| j.data().name() == "Hips") {
@@ -69,21 +69,27 @@ pub fn extract_motion_data(bvh_asset: &Assets<BvhAsset>, build_config: &mut Buil
             trajectory_index += 1;
         }
 
-        motion_data.trajectory_offsets.push(trajectory_data_len);
+        motion_data.pose_offsets.push(motion_data_len);
+        motion_data_len += bvh.num_frames();
+
         motion_data.poses.push(
             bvh.frames()
                 .map(|f| f.as_slice().to_owned())
                 .collect::<Vec<_>>(),
         );
-        motion_data.pose_offset.push(bvh.num_frames());
     }
 
+    motion_data.trajectory_offsets.push(trajectory_data_len);
+    motion_data.pose_offsets.push(motion_data_len);
+
+    // TODO(perf): Serialize into binary instead
     let convert_to_json = serde_json::to_string(&motion_data).unwrap();
 
     let mut motion_library = fs::OpenOptions::new()
         .write(true)
         .create(true)
         .truncate(true)
+        // TODO: specify a file name and possibly a  location
         .open("motion_data.json")
         .unwrap();
 
