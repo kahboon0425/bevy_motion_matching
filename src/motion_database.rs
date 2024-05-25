065c5f44b1b6afd8d688b741896b02edc69a2cd7
kahboon0425 <changkahboon25@gmail.com>
2024-05-25 17:35:55 +0800

    feat: treat motion data as asset

--- src/motion_database.rs
+++ src/motion_database.rs
@@ -1,4 +1,11 @@
-use bevy::prelude::*;
+use bevy::{
+    asset::{io::Reader, AssetLoader, AsyncReadExt, LoadContext},
+    prelude::*,
+    utils::{
+        thiserror::{self, Error},
+        BoxedFuture,
+    },
+};
 use bvh_anim::{Frame, Joint};
 use serde::{Deserialize, Serialize};
 use std::{fs, io::Write};
@@ -11,19 +18,24 @@ use crate::{
 pub struct MotionDatabasePlugin;
 
 impl Plugin for MotionDatabasePlugin {
-    fn build(&self, _app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.init_asset::<MotionDataAsset>()
+            .init_asset_loader::<MotionDataAssetLoader>()
+            .add_systems(Startup, load_motion_data)
+            .add_systems(Update, (check_all_motion_data, check_motion_data));
+    }
 }
 
 pub type Pose = Vec<Vec<f32>>;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct TrajectoryPosition {
     pub position: Vec3,
     pub time: f32,
 }
 
-#[derive(Serialize, Deserialize, Default)]
-pub struct MotionData {
+#[derive(Asset, TypePath, Serialize, Deserialize, Default, Debug)]
+pub struct MotionDataAsset {
     pub trajectories: Vec<TrajectoryPosition>,
     pub trajectory_offsets: Vec<usize>,
     pub poses: Vec<Pose>,
@@ -33,8 +45,89 @@ pub struct MotionData {
 // trajectories: [ bvh0:traj0, bvh0:traj1, bvh0:traj2, bvh1:traj0, bvh1:traj1, bvh2:traj0 ]
 //      offsets: [ 0, 3, 5, 6 ]
 
+#[derive(Default)]
+struct MotionDataAssetLoader;
+
+impl AssetLoader for MotionDataAssetLoader {
+    type Asset = MotionDataAsset;
+    type Settings = ();
+    type Error = MotionDataLoaderError;
+
+    fn load<'a>(
+        &'a self,
+        reader: &'a mut Reader,
+        _settings: &'a (),
+        _load_context: &'a mut LoadContext,
+    ) -> BoxedFuture<'a, Result<Self::Asset, Self::Error>> {
+        Box::pin(async move {
+            println!("kkkskf");
+            let mut bytes = Vec::new();
+
+            reader.read_to_end(&mut bytes).await?;
+
+            let motion_data: MotionDataAsset = serde_json::from_slice(&bytes)?;
+
+            let motion_data_asset = MotionDataAsset {
+                trajectories: motion_data.trajectories,
+                trajectory_offsets: motion_data.trajectory_offsets,
+                poses: motion_data.poses,
+                pose_offsets: motion_data.pose_offsets,
+            };
+
+            Ok(motion_data_asset)
+        })
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &["json"]
+    }
+}
+
+#[non_exhaustive]
+#[derive(Debug, Error)]
+pub enum MotionDataLoaderError {
+    #[error("Could not load json file {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Could not deserialize json data")]
+    Serde(#[from] serde_json::Error),
+}
+
+pub fn load_motion_data(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let handle = asset_server.load::<MotionDataAsset>("motion_data/motion_data.json");
+
+    commands.spawn(handle);
+}
+
+pub fn check_motion_data(
+    mut commands: Commands,
+    motion_data_assets: Res<Assets<MotionDataAsset>>,
+    q_motion_data: Query<(Entity, &Handle<MotionDataAsset>)>,
+) {
+    let Ok((entity, handle)) = q_motion_data.get_single() else {
+        return;
+    };
+
+    if let Some(motion_data) = motion_data_assets.get(handle) {
+        println!("Data: {:?}", motion_data);
+        commands.entity(entity).despawn();
+        commands.entity(entity).remove::<Handle<MotionDataAsset>>();
+    }
+}
+
+pub fn check_all_motion_data(mut motion_data_event: EventReader<AssetEvent<MotionDataAsset>>) {
+    for motion in motion_data_event.read() {
+        match motion {
+            AssetEvent::Added { id } => println!("Loaded: {:?}", id),
+            _ => {} // AssetEvent::Modified { id } => todo!(),
+                    // AssetEvent::Removed { id } => todo!(),
+                    // AssetEvent::Unused { id } => todo!(),
+                    // AssetEvent::LoadedWithDependencies { id } => todo!(),
+        }
+    }
+}
+
 pub fn extract_motion_data(bvh_asset: &Assets<BvhAsset>, build_config: &mut BuildConfig) {
-    let mut motion_data = MotionData::default();
+    let mut motion_data = MotionDataAsset::default();
 
     let mut trajectory_data_len = 0;
     let mut motion_data_len = 0;
@@ -90,7 +183,7 @@ pub fn extract_motion_data(bvh_asset: &Assets<BvhAsset>, build_config: &mut Buil
         .create(true)
         .truncate(true)
         // TODO: specify a file name and possibly a  location
-        .open("motion_data.json")
+        .open("assets/motion_data/motion_data.json")
         .unwrap();
 
     motion_library
